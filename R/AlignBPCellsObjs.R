176e30802c48c86036ef113029b4c775b1277026
jonathan-columbiau <ja3527@cumc.columbia.edu>
2024-01-24 16:55:45 -0500

    Change description

--- R/AlignBPCellsObjs.R
+++ R/AlignBPCellsObjs.R
@@ -11,8 +11,8 @@
 #' bpcells_obj2 = AlignBPCellsObjs(bpcells_obj2, bpcells_obj1)
 AlignBPCellsObjs <- function(bpcells_obj1, bpcells_obj2) {
   #only keep genes with at least 1 cell expressed and found in both datasets
-  genes_expressed_obj1 <- rowSums(bpcells_obj1) %>% .[.!=0] %>% names()
-  genes_expressed_obj2 <- rowSums(bpcells_obj2) %>% .[.!=0] %>% names()
+  genes_expressed_obj1 <- BPCells::rowSums(bpcells_obj1) %>% .[.!=0] %>% names()
+  genes_expressed_obj2 <- BPCells::rowSums(bpcells_obj2) %>% .[.!=0] %>% names()
   keep_genes = intersect(genes_expressed_obj1, genes_expressed_obj2)
   bpcells_obj1 = bpcells_obj1[keep_genes,]
   bpcells_obj1
